9ea53d4a6b15763c999329eeccbb66c74e908f56
Lucky Fox <luckyfox@Luckys-MacBook-Air.local>
2015-09-12 22:39:23 -0400

    Initial Commit

---
+++ sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sample
+//
+//  Created by Lucky Fox on 9/12/15.
+//  Copyright (c) 2015 Lucky Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sample
+//
+//  Created by Lucky Fox on 9/12/15.
+//  Copyright (c) 2015 Lucky Fox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
